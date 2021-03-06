4a7203cf09e091184387178f1a57e08becbe6269
jhonatheberson <jhonatheberson@outlook.com.br>
2021-03-06 20:28:57 -0300

    added computed and watch, dasafio-03

--- dom/desafio-02/app.js
+++ dom/desafio-02/app.js
@@ -1,15 +1,6 @@
 new Vue({
-  el: "#desafio",
-  data: {
-    valor: "",
-  },
-  methods: {
-    exibirAlerta(event) {
-      alert('exibindo alerta"');
-    },
-    armazenarValue(event) {
-      console.log(event.target.value);
-      this.valor = event.target.value;
-    },
-  },
-});
+    el: '#desafio',
+    data: {
+        valor: ''
+    }
+})
